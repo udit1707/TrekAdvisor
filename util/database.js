3b28b5740c59c24958537699b994fd12ffd7eee8
Udit Singh <43987867+udit1707@users.noreply.github.com>
2020-12-30 02:11:57 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,7 +1,4 @@
  const Sequelize = require('sequelize');
- const sequelize=new Sequelize('heroku_32ef109f1d9bc84','b92e5f9aaa362f','cd35a5e2',{dialect:'mysql',host:'us-cdbr-east-02.cleardb.com',storage: "./session.sqlite"});
+ const sequelize=new Sequelize('Database','Username','password',{dialect:'mysql',host:'{Host name}',storage: "./session.sqlite"});
  module.exports=sequelize;
 
-// const Sequelize=require('sequelize');
-// const sequelize=new Sequelize('trek-advisor','root','5511',{dialect:'mysql',host:'localhost',storage: "./session.sqlite"});
-// module.exports=sequelize;
